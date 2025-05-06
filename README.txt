1b1fe6f523ceb351e941c4e17920831299674a5a
Sharma, Anuj <asharma721@gatech.edu>
2025-05-06 12:29:30 -0400

    Initial commit: GBA project files

---
+++ README.txt
@@ -0,0 +1,5 @@
+This objective of this game is to get SpongeBob catch the jellyfish (the magenta square)
+in order to win the game. If spongebob goes through and touches the sea urchins (the black squares),
+then the player loses the game. The basic controls to control SpongeBob is the arrow keys on the keyboard.
+The game starts in the Start page where the user must hit ENTER to begin the game. At any stage in the game, 
+if the user hits BACKSPACE, the user will be returned to the start page of the game.
---
+++ images/player.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=15x15 player player.jpeg 
+ * Time-stamp: Tuesday 04/04/2023, 21:49:58
+ * 
+ * Image Information
+ * -----------------
+ * player.jpeg 15@15
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+extern const unsigned short player[225];
+#define PLAYER_SIZE 450
+#define PLAYER_LENGTH 225
+#define PLAYER_WIDTH 15
+#define PLAYER_HEIGHT 15
+
+#endif
+
